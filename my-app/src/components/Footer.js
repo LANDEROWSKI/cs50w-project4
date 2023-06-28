21fc2f401be06edaa33b2f65dd56916f11485078
LANDEROWSKI <erickjeronimo2468@gmail.com>
2023-06-28 14:06:35 -0600

    Creamos el header y footer en REACT

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container} from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <footer>
+        <Container>Footer</Container>
+
+    </footer>
+  )
+}
+
+export default Footer
