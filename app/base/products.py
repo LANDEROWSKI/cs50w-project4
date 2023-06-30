edac17adca7fd56c96c53b1991188a4df418242b
LANDEROWSKI <erickjeronimo2468@gmail.com>
2023-06-30 09:30:44 -0600

    Este es el mensaje correcto

---
+++ app/base/products.py
@@ -0,0 +1,54 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Camiseta Anime',
+        'image': '/images/camiseta_anime.jpeg',
+        'description':
+        'Camiseta de color blanco con disenio de anime',
+        'brand': 'Unknown',
+        'price': 250,
+        'countInStock': 10,
+        'rating': 4.2,
+        'numReviews': 12,
+
+    },
+    {
+        '_id': '2',
+        'name': 'Figura de Rem',
+        'image': '/images/rem_figure.jpg',
+        'description':
+        'Figura de porcelana de REM del anime Re Zero',
+        'brand': 'Unknown',
+        'price': 1050.00,
+        'countInStock': 4,
+        'rating': 4.8,
+        'numReviews': 5,
+
+    },
+    {
+        '_id': '3',
+        'name': 'Caqueta Ahegao Impresion 3D',
+        'image': '/images/ahegao_jacket.jpg',
+        'description':
+        'Chaqueta con disenio ahegao',
+        'brand': 'Unknown',
+        'price': 560,
+        'countInStock': 18,
+        'rating': 4.0,
+        'numReviews': 8,
+    },
+    {
+        '_id': '4',
+        'name': 'Llaveros de Kimetsu',
+        'image': '/images/anime_llaveros.jpg',
+        'description':
+        'Llaveros de Kimetsu no Yaiba',
+        'brand': 'Unknown',
+        'price': 400,
+        'countInStock': 0,
+        'rating': 4.5,
+        'numReviews': 3,
+    },
+
+]
+
--- my-app/src/screens/HomeScreen.js
+++ my-app/src/screens/HomeScreen.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Row,Col } from 'react-bootstrap'
 
 import Product from '../components/Product'
 import products from '../products'
+import axios from 'axios'
 
 
 function HomeScreen() {
+
+  /* Declaramos una variable de estado llamada products e inicializarla con un arreglo vacío []. Además, 
+  asigna una función setProducts que se utilizará para actualizar el valor de products.*/ 
+  const[products, setProducts] = useState([])
+
+  useEffect(() =>{
+
+    async function fetchProducts(){
+      const {data} = await axios.get('/api/products/')
+      setProducts(data)
+    }
+
+    fetchProducts()
+    
+
+  }, [])
   return (
     <div>
       <h1>Ultimos productos agregados</h1>
